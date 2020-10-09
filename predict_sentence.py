8004b884bbb8918346564fbaa32e6f9f66560881
charvirus <jbrandon413@naver.com>
2020-10-09 19:52:48 +0900

    #6 적중률 다르게 하는거 완료, 실전 적용 밑 비속어 처리 하는지 시험하고 교정하면 됨

--- predict_sentence.py
+++ predict_sentence.py
@@ -12,27 +12,30 @@ from keras.models import Sequential, load_model
 from keras.layers import SimpleRNN, Embedding, Dense, LSTM, Dropout
 from keras.preprocessing.sequence import pad_sequences
 
-
-with open('word_to_index.pickle','rb') as fr:
+with open('word_to_index.pickle', 'rb') as fr:
     dict_loaded = pickle.load(fr)
 
-print(dict_loaded)
 
 loaded_model = load_model('predict_test_model_10-06.h5')
-max_len = 20
+max_len = 30
+print(dict_loaded)
+dict_loaded = Tokenizer(num_words=1000, oov_token="<OOV>")
+# print(dict_loaded)
+test = ['진짜 쓰레기들', '미친 또라이 새끼들']
+test_sequences = dict_loaded.texts_to_sequences(test)
+# print(test_sequences)
+# print(dict_loaded)
 
 
 def predict_sentence(subject_sentence):
-    ftoken = Tokenizer()
-    ftoken.fit_on_texts(subject_sentence)  # 토큰화
-    fword_to_index = ftoken.texts_to_sequences([subject_sentence])
+    dict_loaded.fit_on_texts(subject_sentence)  # 토큰화
+    fword_to_index = dict_loaded.texts_to_sequences([subject_sentence])
     pad_new = pad_sequences(fword_to_index, maxlen=max_len)
     score = float(loaded_model.predict(pad_new))
     if (score > 0.5):
         print("정확도 : {:.2f}%".format(score * 100))
 
 
-predict_sentence('미친 쓰레기들')
-predict_sentence('가해자 인권 중심 집단답네')
-
-predict_sentence('희망')
+predict_sentence('멍청한 것들')
+predict_sentence('또라이 중심 집단답네')
+predict_sentence('민주당은 무슨 공산당이네 ㅋㅋㅋㅋ')
