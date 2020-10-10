95a93155253213542643ab0bc1ba48c57c2fd82a
charvirus <jbrandon413@naver.com>
2020-10-10 15:35:25 +0900

    #7 적중룔 조정(50%), 데이터 500개 학습

--- filtertext.py
+++ filtertext.py
@@ -16,7 +16,7 @@ import pickle
 # wv = model.wv
 
 # 엑셀 파일 경로
-excel_data = pd.read_excel('인공지능 데이터수집 통합본(3).xlsx'
+excel_data = pd.read_excel('runningfile.xlsx'
                            , sheet_name='Sheet1')
 
 # 첫번째 행의 머리말을 따옴
@@ -32,32 +32,32 @@ excel_data = pd.read_excel('인공지능 데이터수집 통합본(3).xlsx'
 # 정규 표현식을 통한 한글 외 문자 제거
 excel_data['내용'] = excel_data['내용'].str.replace("[^ㄱ-ㅎㅏ-ㅣ가-힣 ]", "")
 
+# print('댓글 최대 길이 : ',max(len(l) for l in excel_data['내용']))
+# print('댓글 평균 길이 : ',sum(len(l) for l in excel_data['내용'])/len(excel_data['내용']))
+
 # 문자 제거후 내용 출력
 # for i in excel.index:
 #     print(excel['내용'][i])
 
 # 불용어
 stopwords = ['의', '가', '이', '은', '들', '는', '좀', '잘', '걍', '과', '도', '를', '으로', '자', '에', '와', '한', '하다'
-             ]
+]
 
 excel_data['분류'] = excel_data['분류'].replace(['Clean', 'Bad'], [0, 1])
+
 max_words = 1000
 maxlen = 50
 X_data = excel_data['내용']
 Y_data = excel_data['분류']
-X_len = len(X_data)
-tokenizer = Tokenizer(num_words = max_words, oov_token="<OOV>")
+
+tokenizer = Tokenizer(num_words=max_words, oov_token="<OOV>")
 tokenizer.fit_on_texts(X_data)  # 토큰화
 word_to_index = tokenizer.word_index
 
 sequences = tokenizer.texts_to_sequences(X_data)  # 단어를 숫자값, 인덱스로 변환하여 저장
 
-
-
-# print(word_to_index)  # 단어의 인덱스와 숫자 값을 보여줌
+print(word_to_index)  # 단어의 인덱스와 숫자 값을 보여줌
 # print(X_data[:3])
-print(X_len)
-
 
 flat_X_data = np.array(X_data).flatten().tolist()
 # 여기 밑부터 막힘 학습을 시켜야하는데 어떻게 코딩을 해야할 지 모름 (10-06)
@@ -68,25 +68,23 @@ x = sequence.pad_sequences(sequences, maxlen)
 
 keras.backend.clear_session()
 model = Sequential()
-model.add(Embedding(max_words, 32))
-model.add(LSTM(32))
+model.add(Embedding(max_words, 115))
+model.add(LSTM(115))
 model.add(Dense(1, activation='sigmoid'))
 model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
 
 # batch_size는 변수로 지정할것 예를들어 데이터가 1000개면 그것의 10%정도로 할 것
-history = model.fit(x, Y_data, epochs=100, batch_size=20).history
+history = model.fit(x, Y_data, epochs=200, batch_size=50).history
 
-test = ['진짜 쓰레기들','미친 또라이 새끼들']
+test = ['진짜 쓰레기들', '미친 또라이 새끼들']
 
 test_sequences = tokenizer.texts_to_sequences(test)
 
 print(test_sequences)
 
 # plt.plot(history['loss'], label='loss')
-#
 # plt.legend()
 # plt.show()
-#
 # plt.plot(history['acc'], label='acc')
 # plt.legend()
 # plt.show()
@@ -94,29 +92,18 @@ print(test_sequences)
 # 현재 모델을 파일로 따로 저장함 , (적중률이 높은 모델이면 저장할 것)
 model.save("predict_test_model_10-06.h5")
 
-with open('word_to_index.pickle','wb') as fw:
-    pickle.dump(word_to_index,fw)
-
+with open('word_to_index.pickle', 'wb') as fw:
+    pickle.dump(word_to_index, fw)
 
 # vocab_size = len(word_to_index) + 1
-#
 # n_of_train = int(len(sequences) * 0.8)
 # n_of_test = int(len(sequences) - n_of_train)
-#
 # X_data = sequences
-#
-# X_test = excel_data[n_of_train:]
-# y_test = np.array(Y_data[n_of_train:])
-# X_train = excel_data[:n_of_train]
-# y_train = np.array(Y_data[:n_of_train])
-#
 # model = Sequential()
 # model.add(Embedding(vocab_size, 32)) # 임베딩 벡터의 차원은 32
 # model.add(SimpleRNN(32)) # RNN 셀의 hidden_size는 32
 # model.add(Dense(1, activation='sigmoid'))
-#
 # model.compile(optimizer='rmsprop', loss='binary_crossentropy', metrics=['acc'])
-#
 # history = model.fit(X_train, y_train, epochs=5, batch_size=1024, validation_split=0.2).history
 
 # print('\nko.bin에 있는 내역이 엑셀 데이터에 있는지 찾아줌\n')
@@ -134,22 +121,13 @@ with open('word_to_index.pickle','wb') as fw:
 #     for w in temp_x:
 #         print(w, end=" ")
 #     print()
-
-# 댓글 길이 분포 확인
-# print('댓글 최대 길이 : ',max(len(l) for l in tokenized_data))
-# print('댓글 평균 길이 : ', sum(map(len,tokenized_data))/len(tokenized_data))
-
 # plt.hist([len(s) for s in tokenized_data], bins= 50)
 # plt.xlabel('length of samples')
 # plt.ylabel('number of samples')
 # plt.show()
-
 # model = Word2Vec(sentences=tokenized_data, size=100, window=5, min_count=5, workers=4, sg=0)
-
 # for k in model.wv.vectors:
 #     print(k)
-
 # 완성된 임베딩 매트릭스의 크기 확인
 # print(model.wv.vectors.shape)
-
 # print(model.wv.most_similar("대가리"))
--- predict_sentence.py
+++ predict_sentence.py
@@ -15,27 +15,31 @@ from keras.preprocessing.sequence import pad_sequences
 with open('word_to_index.pickle', 'rb') as fr:
     dict_loaded = pickle.load(fr)
 
-
 loaded_model = load_model('predict_test_model_10-06.h5')
-max_len = 30
+max_len = 50
 print(dict_loaded)
 dict_loaded = Tokenizer(num_words=1000, oov_token="<OOV>")
-# print(dict_loaded)
-test = ['진짜 쓰레기들', '미친 또라이 새끼들']
-test_sequences = dict_loaded.texts_to_sequences(test)
+print(dict_loaded)
+
+
 # print(test_sequences)
 # print(dict_loaded)
 
 
 def predict_sentence(subject_sentence):
-    dict_loaded.fit_on_texts(subject_sentence)  # 토큰화
-    fword_to_index = dict_loaded.texts_to_sequences([subject_sentence])
+    cng_subject_sentence = re.sub('[^ㄱ-ㅎㅏ-ㅣ가-힣 ]', '',subject_sentence)
+    dict_loaded.fit_on_texts(cng_subject_sentence)  # 토큰화
+    fword_to_index = dict_loaded.texts_to_sequences([cng_subject_sentence])
     pad_new = pad_sequences(fword_to_index, maxlen=max_len)
     score = float(loaded_model.predict(pad_new))
-    if (score > 0.5):
-        print("정확도 : {:.2f}%".format(score * 100))
-
+    # if (score > 0.5):
+    print("정확도 : {:.2f}%".format(score))
 
 predict_sentence('멍청한 것들')
 predict_sentence('또라이 중심 집단답네')
 predict_sentence('민주당은 무슨 공산당이네 ㅋㅋㅋㅋ')
+predict_sentence('씨발 이게 나라냐?')
+predict_sentence('맘충')
+predict_sentence('개 병...신들 천지구나')
+predict_sentence('지랄도 풍년이다')
+predict_sentence('이러지 마세요 제발... 나머지 동료들 미운털 박히겠어요...')
